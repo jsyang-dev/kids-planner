2551bb7146823bef03cb78c4c948514d66d38cd4
Jeongsu Yang <mycat83@gmail.com>
2023-04-04 22:41:51 +0900

    Initial commit

---
+++ src/test/java/info/kidsplanner/KidsPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package info.kidsplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidsPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
