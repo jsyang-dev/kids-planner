41c68c5280f7e1feec3a42f073b33ff631ebe8af
Jeongsu Yang <mycat83@gmail.com>
2023-06-24 14:02:28 +0900

    feat(user): 인수 테스트에 Testcontainers 적용
    Closes #4

--- api/src/test/java/info/kidsplanner/AcceptanceTest.java
+++ api/src/test/java/info/kidsplanner/AcceptanceTest.java
@@ -4,11 +4,19 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
 public abstract class AcceptanceTest {
     @LocalServerPort
     private int port;
+
+    @SuppressWarnings("rawtypes")
+    @Container
+    protected static final MySQLContainer MY_SQL_CONTAINER = new MySQLContainer("mysql:8");
 
     @BeforeEach
     protected void setUp() {
