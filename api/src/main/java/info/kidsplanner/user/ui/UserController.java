6b977233bd86fa1d74ad3246dab5186051f4aee6
Jeongsu Yang <mycat83@gmail.com>
2023-07-09 20:41:15 +0900

    feat(user): Webflux 적용
    Closes #21

--- api/src/main/java/info/kidsplanner/user/ui/UserController.java
+++ api/src/main/java/info/kidsplanner/user/ui/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
 import java.net.URI;
@@ -20,9 +21,9 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<UserResponse> create(@RequestBody @Valid UserRequest userRequest) {
+    public Mono<ResponseEntity<UserResponse>> create(@RequestBody @Valid UserRequest userRequest) {
         final UserResponse userResponse = userService.create(userRequest);
         final URI uri = URI.create("/users/" + userResponse.getId());
-        return ResponseEntity.created(uri).body(userResponse);
+        return Mono.just(ResponseEntity.created(uri).body(userResponse));
     }
 }
