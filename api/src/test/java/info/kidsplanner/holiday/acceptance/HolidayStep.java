801327512bfc0071fa2303133ac1390550e7a22d
Jeongsu Yang <mycat83@gmail.com>
2023-07-23 20:54:53 +0900

    feat(holiday): 공휴일 인수 테스트
    Refs #23

---
+++ api/src/test/java/info/kidsplanner/holiday/acceptance/HolidayStep.java
@@ -0,0 +1,20 @@
+package info.kidsplanner.holiday.acceptance;
+
+import info.kidsplanner.AcceptanceStep;
+import info.kidsplanner.holiday.application.dto.HolidayRequest;
+import info.kidsplanner.util.AcceptanceUtil;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class HolidayStep extends AcceptanceStep {
+    public static final String HOLIDAY_URI = "/holidays";
+
+    public static ExtractableResponse<Response> 공휴일_생성(HolidayRequest holidayRequest) {
+        return 공휴일_생성(givenSpec(), holidayRequest);
+    }
+
+    public static ExtractableResponse<Response> 공휴일_생성(RequestSpecification specification, HolidayRequest holidayRequest) {
+        return AcceptanceUtil.invokeHttpPost(specification, HOLIDAY_URI, holidayRequest);
+    }
+}
