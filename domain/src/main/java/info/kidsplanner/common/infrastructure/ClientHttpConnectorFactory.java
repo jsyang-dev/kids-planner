13bf7a6931956f1968c8d0e076827954dd18fba0
Jeongsu Yang <mycat83@gmail.com>
2023-08-10 23:09:48 +0900

    refactor(holiday): 메서드명 변경, 주석 추가
    Refs #23

--- domain/src/main/java/info/kidsplanner/common/infrastructure/ClientHttpConnectorFactory.java
+++ domain/src/main/java/info/kidsplanner/common/infrastructure/ClientHttpConnectorFactory.java
@@ -13,12 +13,14 @@ import java.time.Duration;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ClientHttpConnectorFactory {
     public static ReactorClientHttpConnector from(ConnectionProperties connectionProperties) {
-        return new ReactorClientHttpConnector(httpClient(connectionProperties));
+        return new ReactorClientHttpConnector(createHttpClient(connectionProperties));
     }
 
-    private static HttpClient httpClient(ConnectionProperties connectionProperties) {
-        return HttpClient
-                .create()
+    /**
+     * Connection Timeout과 Response Timeout 설정을 위한 HttpClient 생성
+     */
+    private static HttpClient createHttpClient(ConnectionProperties connectionProperties) {
+        return HttpClient.create()
                 .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, connectionProperties.getConnectionTimeout())
                 .responseTimeout(Duration.ofMillis(connectionProperties.getResponseTimeout()))
                 .wiretap("reactor.netty.http.client.HttpClient", LogLevel.INFO, AdvancedByteBufFormat.TEXTUAL);
