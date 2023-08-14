763c5ffc47a104ff50eaf78ae12eb56dc05b0d0a
Jeongsu Yang <mycat83@gmail.com>
2023-08-14 23:30:26 +0900

    refactor(holiday): HolidayResponse 팩터리 메서드 추가
    Refs #27

--- api/src/main/java/info/kidsplanner/holiday/application/dto/HolidayResponse.java
+++ api/src/main/java/info/kidsplanner/holiday/application/dto/HolidayResponse.java
@@ -1,5 +1,6 @@
 package info.kidsplanner.holiday.application.dto;
 
+import info.kidsplanner.holiday.domain.Holiday;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -10,4 +11,11 @@ import java.time.LocalDate;
 public class HolidayResponse {
     private final LocalDate date;
     private final String name;
+
+    public static HolidayResponse of(Holiday holiday) {
+        return builder()
+                .date(holiday.getDate())
+                .name(holiday.getName())
+                .build();
+    }
 }
